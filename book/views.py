185e3f507d0c800a83be371249d19f68b9737903
khenryjoe <joseph.ketterer@gmail.com>
2020-05-27 11:07:56 +0100

    book urls

--- book/views.py
+++ book/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Book
+
+def index(request):
+    books = Book.objects.all().order_by('-list_date')
